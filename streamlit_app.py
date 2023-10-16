6a696250cdea7947fc5e6a85d36df21b9028f9f0
Anbui <ammchannle@gmail.com>
2023-10-16 20:18:15 +0000

    Pending changes exported from your codespace

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,11 @@ import streamlit as st
 import requests
 def send_noti(content_noti):
      r = requests.post("https://api.pushover.net/1/messages.json", data = {
-       "token": "at71jcurvvpgp9otn212xbkmkpebto",
-       "user": "utrpuktmbuiix9oe7a957od99t45jk",
+        "token": st.secrets["DB_token"],
+        "user": st.secrets["DB_user"],
        "message": content_noti
      })
-def send_noti_img(content_noti,img):
-    r = requests.post("https://api.pushover.net/1/messages.json", data={
-        "token": "at71jcurvvpgp9otn212xbkmkpebto",
-        "user": "utrpuktmbuiix9oe7a957od99t45jk",
-        "message": content_noti
-    },
-    files={
-        "attachment": ('image.jpg', open(img, "rb"), "image/jpeg")
-    })
-action_op = st.sidebar.selectbox('Hôm nay em muốn làm gì nè',('Gửi thông báo cho anh','Làm nhiệm vụ (Coming soon)','Đổi quà (Coming soon)', 'Tiến Trình tích điểm bida (400đ để mở khóa)'))
+action_op = st.sidebar.selectbox('Hôm nay em muốn làm gì nè',('Gửi thông báo cho anh','Làm nhiệm vụ (Coming soon)','Đổi quà (Coming soon)', 'Tích điểm bida (400đ unlock)'))
 if action_op == 'Gửi thông báo cho anh':
     st.header('Gửi thông báo cho anh')
     option = st.selectbox(
@@ -24,10 +15,7 @@ if action_op == 'Gửi thông báo cho anh':
     if option == 'Có':
         img_upload = st.file_uploader("Up file ở đây nè")
     if st.button('Gửi 💀'):
-        if option == 'hong':
-            send_noti('iuuu em ')
-        else:
-            send_noti_img('iuu em',img_upload)
+        send_noti('iuuu em ')
         st.write('Thông báo đã được gửi')
     else:
         st.write('Mún gì ghi vô nè 😼')
